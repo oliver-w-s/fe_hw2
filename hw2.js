75415c044bcd3292449c9a6c39915f7d037ee282
oliver-w-s <oliver.work.sjc@gmail.com>
2022-03-02 19:24:29 -0500

    task123

---
+++ hw2.js
@@ -0,0 +1,21 @@
+const itemsObject = [
+  { quantity: 1, price: 200 },
+  { quantity: 3, price: 350 },
+  { quantity: 5, price: 400 },
+];
+
+let task1 = itemsObject.map(function (obj) {
+  let res = {};
+  res.quantity = obj.quantity * 2;
+  res.price = obj.price * 2;
+  return res;
+});
+console.log(task1);
+
+let task2 = itemsObject.filter(function (obj) {
+  return obj.quantity > 2 && obj.price > 300;
+});
+console.log(task2);
+
+let task3 = itemsObject.reduce((acc, obj) => acc + obj.quantity * obj.price, 0);
+console.log(task3);
